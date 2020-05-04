7ac13066d3ccc8b966ba21c01201c58ce226f6e5
Carlos de Dios <carlos.ferreras@avantica.net>
2020-05-04 10:27:04 -0600

    Fixes to the environment
    - Ensured we can connect client's env with server's env
    - Updated babelrc to keep styled components working (we can remove if we
    want to)
    - Update environment to include API Url

--- next.config.js
+++ next.config.js
@@ -1,21 +1,35 @@
 const webpack = require('webpack');
 
-module.exports = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.module.rules.push({
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        include: [/src/],
-        exclude: ['/node_modules/', '/.next/', '/dist/'],
-        enforce: 'pre',
-        options: {
-          emitWarning: true,
-          fix: true,
-        },
-      });
-    }
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
 
-    return config;
-  },
+module.exports = phase => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
+
+  return {
+    // Properties set here will be available on the client-side via `process.env.[FLAG_NAME]`
+    env: {
+      API_URL: process.env.API_URL
+    },
+    webpack: (config, { dev }) => {
+      if (dev) {
+        config.module.rules.push({
+          test: /\.js$/,
+          loader: 'eslint-loader',
+          include: [/src/],
+          exclude: ['/node_modules/', '/.next/', '/dist/'],
+          enforce: 'pre',
+          options: {
+            emitWarning: true,
+            fix: true,
+          },
+        });
+      }
+  
+      return config;
+    },
+  };
 };
